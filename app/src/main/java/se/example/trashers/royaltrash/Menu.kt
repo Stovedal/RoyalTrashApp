539e873f74a3eadf3481b652faec5e2bfd62a464
Sofia Tovedal <sofiatovedal@Sofias-MacBook-Pro.local>
2018-09-19 11:29:49 +0200

    project created

---
+++ app/src/main/java/se/example/trashers/royaltrash/Menu.kt
@@ -0,0 +1,12 @@
+package se.example.trashers.royaltrash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}

48ecda7d5a1db7b011c7cfc96e183921f5c74453
Co2p <go.co2p@gmail.com>
2018-09-19 12:35:00 +0200

    Refactoring

---
+++ app/src/main/java/se/example/trashers/royaltrash/QuizActivity.kt
@@ -0,0 +1,12 @@
+package se.example.trashers.royaltrash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}

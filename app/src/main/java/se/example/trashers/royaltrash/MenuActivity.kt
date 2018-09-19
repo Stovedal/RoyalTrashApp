b5a3fb46bb6591079c25d46b8b8331a55a0abe96
Co2p <go.co2p@gmail.com>
2018-09-19 14:33:39 +0200

    Start Button intent to Quiz

--- app/src/main/java/se/example/trashers/royaltrash/MenuActivity.kt
+++ app/src/main/java/se/example/trashers/royaltrash/MenuActivity.kt
@@ -1,12 +1,20 @@
 package se.example.trashers.royaltrash
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        var playButton: Button = findViewById(R.id.start_button);
+
+        playButton.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            intent.putExtra("key", "Kotlin")
+            startActivity(intent)
+        }
     }
 }

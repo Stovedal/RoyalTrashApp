a7fa71441f8161abdd8c3717de32c1f58b32ccff
Gordon Cooper <go.co2p@gmail.com>
2018-09-22 00:37:36 +0200

    mer minimalistisk bakgrundsvideo

--- app/src/main/java/se/example/trashers/royaltrash/VideoBgActivity.kt
+++ app/src/main/java/se/example/trashers/royaltrash/VideoBgActivity.kt
@@ -12,7 +12,7 @@ class VideoBgActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_bg)
-        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.earth)
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.flatearth)
         BGVideo.setVideoURI(uri)
         BGVideo.start()
         BGVideo.setOnPreparedListener(MediaPlayer.OnPreparedListener { mediaPlayer -> mediaPlayer.isLooping = true })
@@ -37,7 +37,7 @@ class VideoBgActivity : AppCompatActivity() {
      */
     public override fun onResume() {
         super.onResume()
-        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.earth)
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.flatearth)
         BGVideo.setVideoURI(uri)
         BGVideo.start()
         BGVideo.setOnPreparedListener { mediaPlayer -> mediaPlayer.isLooping = true }

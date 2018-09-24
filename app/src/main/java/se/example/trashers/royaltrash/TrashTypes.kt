774738af08b733d66b7a5109ca8f236ab0e82352
Co2p <go.co2p@gmail.com>
2018-09-24 14:37:56 +0200

    mer ikoner

--- app/src/main/java/se/example/trashers/royaltrash/TrashTypes.kt
+++ app/src/main/java/se/example/trashers/royaltrash/TrashTypes.kt
@@ -2,11 +2,11 @@ package se.example.trashers.royaltrash
 
 enum class TrashTypes(var TrashTypeIcons: List<String>) {
 
-    Glass(listOf("glasobjekt1","glasobjekt1")),
-    Organic(listOf("organisktobjekt1","organisktobjekt1")),
-    Paper(listOf("pappersobjekt1","pappersobjekt1")),
-    Ewaste(listOf("elektronikobjekt1","elektronikobjekt1")),
-    Metal(listOf("metallobjekt1","metallobjekt1")),
-    Plastic(listOf("plastobjekt1","plastobjekt1"))
+    Glass(listOf("glasobjekt1","glasobjekt2")),
+    Organic(listOf("organisktobjekt1","organisktobjekt2")),
+    Paper(listOf("pappersobjekt1","pappersobjekt2")),
+    Ewaste(listOf("elektronikobjekt1","elektronikobjekt2")),
+    Metal(listOf("metallobjekt1","metallobjekt2")),
+    Plastic(listOf("plastobjekt1","plastobjekt2"))
 
 }

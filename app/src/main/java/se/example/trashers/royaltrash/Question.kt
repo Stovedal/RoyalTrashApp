b5a8abd5a0acdcdcb261f5efc22957e827bf5751
Co2p <go.co2p@gmail.com>
2018-10-12 16:31:53 +0200

    Kotlinifiering

--- app/src/main/java/se/example/trashers/royaltrash/Question.kt
+++ app/src/main/java/se/example/trashers/royaltrash/Question.kt
@@ -2,39 +2,11 @@ package se.example.trashers.royaltrash
 
 class Question() {
 
-    var q = DBrequests().apiGetRandomQuestion()
+    var q = DBrequests().getRandomQuestion()
 
-    var questionId:Int = 0
-    var question:String = ""
-    var answer:String = ""
-    var falseAnswers: List<String> = emptyList()
-
-    constructor(numberOfAnswers:Int) : this() {
-        questionId = getId()
-        question = getQuestion(questionId)
-        answer = getAnswer(questionId)
-        falseAnswers = getFalseAnswers(questionId, numberOfAnswers)
-    }
-
-
-    fun getId():Int {
-        //todo get an actual id from the database
-        return q!!.q_id
-    }
-
-    fun getQuestion(questionId: Int): String {
-        //todo get an actual question from the database
-        return q!!.question
-    }
-
-    fun getAnswer(questionId: Int): String {
-        //todo get a real answer
-        return q!!.answer1
-    }
-
-    fun getFalseAnswers(questionId: Int, amount: Int): List<String> {
-        //todo get false answers from database
-        return listOf(q!!.answer2, q!!.answer3, q!!.answer4)
-    }
+    var questionId:Int = q.q_id
+    var question:String = q.question
+    var answer:String = q.answer
+    var falseAnswers: List<String> = listOf(q.alternative1, q.alternative2, q.alternative3)
 
 }

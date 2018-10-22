63ade8b7eeb4b73fc59f3d4a5a01ef409b272b77
Co2p <go.co2p@gmail.com>
2018-10-22 18:15:56 +0200

    Uppdaterade och nya ikoner

--- questionPoster.py
+++ questionPoster.py
@@ -2,11 +2,11 @@ import requests
 
 r = requests.post("http://royaltrashapp.azurewebsites.net/api/Quiz/PostQuiz",
                   json={
-                      "question": "FN:s deklaration om de mänskliga rättigheterna antogs år",
-                      "C1answer": "1948",
-                      "C2answer": "1925",
-                      "C3answer": "1994",
-                      "C4answer": "2001"
+                      "question": "Om man förbränner 1 kilo bensin ger det upphov till...",
+                      "C1answer": "cirka 3 kilo koldioxid",
+                      "C2answer": "cirka 1 kilo koldioxid",
+                      "C3answer": "cirka 5 kilo koldioxid",
+                      "C4answer": "cirka 10 kilo koldioxid"
                         })
 
 print(r.status_code, r.reason)

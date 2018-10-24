4b813650f70443c443dbf6c76fd1947050248efe
Co2p <go.co2p@gmail.com>
2018-10-24 16:15:04 +0200

    Mindre

--- questionPoster.py
+++ questionPoster.py
@@ -2,11 +2,11 @@ import requests
 
 r = requests.post("http://royaltrashapp.azurewebsites.net/api/Quiz/PostQuiz",
                   json={
-                      "question": "Hur lång tid tar det för en flaska att brytas ned av naturen?",
-                      "C1answer": "4000 år",
-                      "C2answer": "300 år",
-                      "C3answer": "25000 år",
-                      "C4answer": "10000 år"
+                      "question": "Hur lång tid tar det för en glasflaska att brytas ned av naturen?",
+                      "C1answer": "",
+                      "C2answer": "",
+                      "C3answer": "",
+                      "C4answer": ""
                         })
 
 print(r.status_code, r.reason)
